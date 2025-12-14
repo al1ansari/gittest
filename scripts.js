e7abaa3200b10676672ac14f42f2345ee7c0b0c3
al1ansari <ali.ansarikamran@gmail.com>
2025-12-14 12:15:40 +0530

    console process.env message updated

--- scripts.js
+++ scripts.js
@@ -1,2 +1,6 @@
 console.log("Hello, World!");
 console.log("This is a test script.");
+console.log("Goodbye, World!");
+console.log("process.env.secret =", process.env.secret);
+console.log("commit amend test");
+console.log("Another line for testing.");
